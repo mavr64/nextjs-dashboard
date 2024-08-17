41220daef40d4d006cbea01829952efda08b9d86
Влад <45437497+mavr64@users.noreply.github.com>
2024-08-17 16:05:30 +0400

    Commit 17.08.24

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+import * as console from "console";
+
+export default function Page() {
+    console.log('OPEN /dashboard/invoices успешно');
+    return (
+        <div>
+            <p>Invoices Page</p>
+        </div>
+    );
+}
